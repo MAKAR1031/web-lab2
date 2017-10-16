1d7905268d3071469789acc60d5ef7ff094f335c
Sergey Makarov <makar1031@yandex.ru>
2017-10-16 22:11:46 +0300

    выполнены общие задания выполнено индивидуальное задание

--- index.js
+++ index.js
@@ -1,37 +1,56 @@
 require('./app.scss')
 
-var Vue = require('vue/dist/vue.js');
+import Vue from 'vue/dist/vue.js'
+import alertify from 'alertify.js'
 
-Vue.component('todo-item', {
-    props: ['todo'],
+const counter = {
+    props: ['count'],
     template: `
-    <li>{{todo.text}}</li>
-  `
-});
+    <div>
+        <p>Total todos: {{count}}</p>
+    </div>
+    `
+}
 
-Vue.component('todo-input', {
-    data: function () {
+const item = {
+    props: ['i', 'todo'],
+    methods: {
+        togglePriority: function () {
+            this.todo.priority = !this.todo.priority;
+        },
+        removeTask: function () {
+            this.$emit('remove', this.i);
+        }
+    },
+    template: `
+    <li>
+        <span>{{ i + 1 }})</span>
+        <span v-bind:class="{'priority-task': todo.priority}">{{ todo.text }}</span>
+        <button class='btn priority' @click='togglePriority'>Priority</button>
+        <button class='btn remove' @click='removeTask'>Remove</button>
+    </li>`
+}
+
+const input = {
+    props: ['disabled'],
+    data: () => {
         return {
             newTodo: ''
-        };
+        }
     },
     methods: {
         onClick: function () {
-            if (this.newTodo === '') {
-                alert('Необходимо значение');
-                return;
-            }
             this.$emit('click', this.newTodo);
             this.newTodo = ''
         }
     },
     template: `
     <div>
-      <input v-model='newTodo' />
-      <button v-on:click='onClick'>Add</button>
+      <input id="title" v-model='newTodo' @keyup.enter='onClick' :disabled='disabled'/>
+      <button class='btn add' @click='onClick' :disabled='disabled'>Add</button>
     </div>
   `
-});
+}
 
 var app = new Vue({
     el: '#app',
@@ -40,10 +59,33 @@ var app = new Vue({
     },
     methods: {
         addTodo: function (text) {
-            console.log(this.todos);
+            if (text === '') {
+                alertify.alert('First you need to enter the text of the task!');
+                return;
+            }
+            for (let i = 0; i < this.todos.length; i++) {
+                if (text === this.todos[i].text) {
+                    alertify.alert("You have already added such a task!");
+                    return;
+                }
+            }
             this.todos.push({
-                text: text
+                text: text,
+                priority: false
             });
+        },
+        removeTodo: function (index) {
+            this.todos.splice(index, 1);
         }
+    },
+    computed: {
+        inputDisabled: function () {
+            return this.todos.length >= 10;
+        }
+    },
+    components: {
+        'todo-item': item,
+        'todo-counter': counter,
+        'todo-input': input,
     }
 });
